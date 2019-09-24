7c1908333e6235126e8c960b069d4e96f74a8f4c
Sam Doggett <sdoggett@awarex.com>
2019-09-24 16:45:40 +0200

    Updated Menu

---
+++ Assets/Scripts/Animator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator : MonoBehaviour
+{
+    public void Animate(Sprite[] sprites, SpriteRenderer spriteRenderer, float framesPerSecond)
+    {
+        StopAllCoroutines();
+        StartCoroutine(AnimateLoop(sprites, spriteRenderer, framesPerSecond));
+    }
+
+    private IEnumerator AnimateLoop(Sprite[] sprites, SpriteRenderer spriteRenderer, float framesPerSecond)
+    {
+        while (true)
+        {
+            foreach (Sprite sprite in sprites)
+            {
+                spriteRenderer.sprite = sprite;
+                yield return new WaitForSeconds(1 / framesPerSecond);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DinoAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoAnimator : Animator
+{
+    public Sprite[] idleSprites;
+    public Sprite[] runningSprites;
+    public float framesPerSecond = 20;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        RunningAnimation();
+    }
+
+    public void IdleAnimation()
+    {
+        Animate(idleSprites, spriteRenderer, idleSprites.Length / framesPerSecond);
+    }
+
+    public void RunningAnimation()
+    {
+        Animate(runningSprites, spriteRenderer, runningSprites.Length / framesPerSecond);
+    }
+}
