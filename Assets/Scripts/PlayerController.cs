b2f43ec6e69210f340f87a606cdb70811240a2b2
Sam Doggett <sdoggett@awarex.com>
2019-09-25 13:24:36 +0200

    Swipe gesture

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private Vector2 initialPosition;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                initialPosition = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                Vector2 direction = touch.position - initialPosition;
+                if (direction.y > 0)
+                {
+                    print("Spawn bump");
+                }
+                else if (direction.y < 0)
+                {
+                    print("Spawn dip");
+                }
+            }
+        }
+    }
+}
