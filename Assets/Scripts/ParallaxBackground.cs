6a4d4a506246a2743ba65b605fab4b3456d90e50
Sam Doggett <sdoggett@awarex.com>
2019-09-26 17:50:49 +0200

    Added parallax effect

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Tutorial: https://www.youtube.com/watch?v=zit45k6CUMk
 public class ParallaxBackground : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float parallaxEffect;
+    public GameObject cam;
+
+    private float length;
+    private float startPos;
+
     void Start()
     {
-        
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float temp = cam.transform.position.x * (1 - parallaxEffect);
+        float distance = cam.transform.position.x * parallaxEffect;
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if(temp > startPos + length)
+        {
+            startPos += length;
+        } else if (temp < startPos - length)
+        {
+            startPos -= length;
+        }
     }
 }
