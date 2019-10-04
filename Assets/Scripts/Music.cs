bbf680c1369f0b543da075f2422e3f9b26dd5e30
Jasper Gordon <jgordon3@bowdoin.edu>
2019-10-04 12:54:36 +0200

    Working on Music and buttons for Settings

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Music : MonoBehaviour
+{
+	private static Music audioSource = null;
+    public static Music Instance
+	{
+        get { return audioSource; }
+	}
+
+    private void Awake()
+	{
+		if (audioSource != null && audioSource != this) {
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			audioSource = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
---
+++ Assets/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Settings : MonoBehaviour
+{
+
+    public Button musicOff;
+    public Button musicOn;
+    public Button soundEffectsOn;
+    public Button soundEffectsOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicOff.onClick.AddListener(MusicOffButtonClicked);
+    }
+
+    void MusicOffButtonClicked()
+    {
+        Music.Instance
+    }
+}
