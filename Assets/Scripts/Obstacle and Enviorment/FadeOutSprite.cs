93211ecb5f439cb0bdff74969fa070e7f6aa17e6
Sam Doggett <sdoggett@awarex.com>
2019-10-20 17:27:33 +0200

    Fading background

---
+++ Assets/Scripts/Obstacle and Enviorment/FadeOutSprite.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutSprite : MonoBehaviour
+{
+    public float fadeTime = 10f;
+    public float fadeRate = 1f;
+    private float fadeAmount;
+    private SpriteRenderer spriteRenderer;
+    private Color fadeColor;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        fadeColor = spriteRenderer.color;
+        fadeAmount = 1f / fadeTime;
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(fadeRate);
+
+        fadeTime = fadeTime - fadeRate;
+
+        fadeColor.a = fadeColor.a - fadeAmount;
+
+        spriteRenderer.color = fadeColor;
+
+        if (fadeTime >= 0)
+        {
+            StartCoroutine(FadeOut());
+        }
+    }
+}
