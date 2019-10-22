beab919d94e7155abb4a117bc40202d0d0939af2
Caleb Eurich <calebeurich27@gmail.com>
2019-10-22 10:30:22 +0200

    updated meteorGenerator with track percentage
    can set the percentage of meteors that track the dino vs go straight down

--- Assets/Scripts/Obstacle and Enviorment/MeteorGenerator.cs
+++ Assets/Scripts/Obstacle and Enviorment/MeteorGenerator.cs
@@ -8,10 +8,11 @@ public class MeteorGenerator : MonoBehaviour
     
     public GameObject targetObj;
     public GameObject meteorPre;
-    public float meteorTime = 1;
-    public float range = 10;
-    public float minSize = 0.5f;
-    public float maxSize = 0.75f;
+    public float meteorTime;
+    public float range;
+    public float minSize;
+    public float maxSize;
+    public float trackPercentage;
 
     void Start()
     {
@@ -19,6 +20,15 @@ public class MeteorGenerator : MonoBehaviour
     }
 
     IEnumerator MeteorGenerate() {
+        int rInt = Random.Range(0, 100);
+        bool goDown = true;
+
+        if (rInt < trackPercentage)
+        {
+            goDown = false;
+        }
+
+        
         GameObject clone;
         Vector3 spawnPosition = transform.position;
         spawnPosition += new Vector3(Random.Range(-range, range), 0, 0);
@@ -37,12 +47,24 @@ public class MeteorGenerator : MonoBehaviour
         float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
         clone.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
         clone.transform.Rotate(0, 0, 90, Space.World);
-
+    
         Rigidbody2D rb = clone.GetComponent<Rigidbody2D>();
 
-        Vector2 meteorVector = (Vector2)targetObj.transform.position - rb.position;
+        Vector2 meteorVector = new Vector2(0,0);
+        if (!goDown)
+        {
+            meteorVector = (Vector2)targetObj.transform.position - rb.position;
+            Debug.Log("should track");
+        }
+        else
+        {
+            meteorVector.y = -1;
+            Debug.Log("should go down");
+        }
+
         meteorVector.Normalize();
         rb.velocity = meteorVector * (GameManager.instance.tileMovementSpeed);
+
 
         yield return new WaitForSeconds(meteorTime);
         StartCoroutine(MeteorGenerate());
